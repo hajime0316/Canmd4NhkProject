f2647b9009ee520641c7db075fa08756207c28fd
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-08-25 22:47:29 +0900

    Add TIM interrupt routine

--- Src/cppmain.cpp
+++ Src/cppmain.cpp
@@ -47,6 +47,88 @@ void loop(void) {
 }
 
 //**************************
+//    タイマ割り込み関数
+//**************************
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	// 10msecタイマ
+	if(htim->Instance == TIM6) {
+		int motor_control_data[2] = {};
+        double duty_rate[2];
+
+		// モータコントロールデータ取得
+		canmd_manager_get_motor_control_data(motor_control_data);
+
+        // モーターセットアップデータの取得
+        MotorSetupData motor_setup_data[2];
+        canmd_manager_get_motor_setup_data(motor_setup_data);
+/*
+        // PIDモジュールを作成
+        static Pid pid_module[2] = {
+            {(double)motor_setup_data[0].kp, (double)motor_setup_data[0].ki, (double)motor_setup_data[0].kd},
+            {(double)motor_setup_data[1].kp, (double)motor_setup_data[1].ki, (double)motor_setup_data[1].kd}
+        };
+
+        // velocityモジュールの作成
+        static Stm32Velocity velocity_module[2] = {{&htim2, 0}, {&htim3, 0}};
+        // 速度計算
+        for (int i = 0; i < 2; i++){
+            g_velocity[i] = velocity_module[i].periodic_calculate_velocity();
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            //// モーターコントロールモードによってduty比の決め方を分ける
+            switch (motor_setup_data[i].control_mode)
+            {
+            case DUTY_RATE_MODE:
+                duty_rate[i] = motor_control_data[i] / (double)MOTOR_CONTROL_DATA_MAX;
+                break;
+            
+            case PID_MODE:
+                // PID制御の計算
+                int pid_output;
+                pid_output = pid_module[i].pid_calc(velocity_module[i].get_velocity(), motor_control_data[i]);
+
+                // duty比計算
+                duty_rate[i] = pid_output / (double)PID_OUTPUT_MAX;
+                
+                break;
+
+            default:
+                duty_rate[i] = 0;
+                break;
+            }
+            
+        }
+*/
+        // TODO: 上のコメントアウトを消して以下のコードを削除
+        for (int i = 0; i < 2; i++)
+        {
+            duty_rate[i] = motor_control_data[i] / (double)MOTOR_CONTROL_DATA_MAX;
+        }
+        // TODO: ここまで削除
+
+		// PWMのデューティー比更新
+        static Stm32AntiphasePwm pwm0(&htim3, TIM_CHANNEL_4, TIM_CHANNEL_3);
+        static Stm32AntiphasePwm pwm1(&htim3, TIM_CHANNEL_1, TIM_CHANNEL_2);
+
+        pwm0.update_duty(duty_rate[0]);
+        pwm1.update_duty(duty_rate[1]);
+	}
+
+	// 100msecタイマ
+	if(htim->Instance == TIM7) {
+		if(canmd_manager_time_out_check()) {
+			HAL_GPIO_WritePin(LED_GP_GPIO_Port, LED_GP_Pin, GPIO_PIN_SET);
+		}
+		else {
+			HAL_GPIO_TogglePin(LED_GP_GPIO_Port, LED_GP_Pin);
+		}
+	}
+}
+
+//**************************
 //    CAN通信受信割り込み
 //**************************
 void stm32_easy_can_interrupt_handler(void)
